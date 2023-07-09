20d36dc81a77069d6ea46c6f7338dc9479dccfe2
x-mas87 <dlkst@naver.com>
2023-07-09 16:28:01 +0900

    first commit

---
+++ src/test/java/com/example/portfolio_1/Portfolio1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.portfolio_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Portfolio1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
